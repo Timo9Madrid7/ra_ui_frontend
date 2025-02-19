1a4eb321bd1090bbfe41b60481cb3ce6c164510e
dhimashary <dhimas.hary@gmail.com>
2025-02-19 16:26:48 +0100

    add loading state on play auralization

--- src/components/UI/Results/components/Plots/AuralizationPlot/AuralizationPlot.tsx
+++ src/components/UI/Results/components/Plots/AuralizationPlot/AuralizationPlot.tsx
@@ -1,10 +1,10 @@
-import { Avatar, IconButton, List, ListItem, ListItemAvatar, ListItemText } from "@mui/material";
+import { Avatar, IconButton, List, ListItem, ListItemAvatar, ListItemText, CircularProgress } from "@mui/material";
 import classes from "../styles.module.scss";
 import axios from '@/client';
 import { useLocation } from 'react-router-dom';
 
 /** Components */  
-import { useGetAudios } from '@/hooks/Auralization/useAudioOption';
+import { useGetAudios, useGetAuralizationStatus } from '@/hooks/Auralization';
 import PlayCircleIcon from '@mui/icons-material/PlayCircle';
 import FolderIcon from '@mui/icons-material/Folder';
 import { SelectAutoComplete } from "@/components/Base/Select/SelectAutoComplete";
@@ -19,8 +19,10 @@ export const AuralizationPlot = ({
     index: number,
 }) => {
     const [selectedAudioOption, setSelectedAudioOption] = useState<AnechoicOption | null>(null);
-    
-    
+    const [loadingAuralization, setLoadingAuralization] = useState<boolean>(false);
+    const [auralizationStatus, setAuralizationStatus] = useState<string>('');
+    const [auralizationId, setAuralizationId] = useState<number>(0);
+
     const location = useLocation();
     const queryParams = new URLSearchParams(location.search);
     const simulationId = queryParams.get('simulationId');
@@ -33,6 +35,26 @@ export const AuralizationPlot = ({
             }
         }
     }, [audioOptions, isLoadingAudioOptions, audioOptionsError]);
+
+    useEffect(() => {
+        if (auralizationStatus === "Completed") {
+            setLoadingAuralization(false);
+        }
+    }, [auralizationStatus]);
+
+    const { data: checkAuralizationResult } = useGetAuralizationStatus(
+        auralizationId,
+        auralizationStatus !== "" && auralizationStatus !== "Completed" && auralizationId !== 0,
+        5000
+    );
+
+    useEffect(() => {
+        if (checkAuralizationResult) {
+            if (checkAuralizationResult.status === "Completed") {
+                setAuralizationStatus(checkAuralizationResult.status);
+            }
+        }
+    }, [checkAuralizationResult]);
 
     if (audioOptionsError) {
         console.log(audioOptionsError);
@@ -53,18 +75,20 @@ export const AuralizationPlot = ({
 
     const postAuralization = async ({simulationId, audioOptionId}: {simulationId: string, audioOptionId: number}) => {
         console.log('Post auralization', simulationId, audioOptionId);
+        setAuralizationStatus('');
+        setLoadingAuralization(true);
         try {
             const response = await axios.post('/auralizations', {
                 "audioFileId": audioOptionId,
                 "simulationId": simulationId,
             });
-            console.log(response.data)
+            setAuralizationStatus(response.data.status);
+            setAuralizationId(response.data.id);
         } catch (error) {
             console.log(error);
         }
     };
     
-
     return <div className={classes.plot_container} style={{display: value === index ? 'block' : 'none'}}>
         <List>
             <h2 className={classes.plot_header}>Auralization</h2>
@@ -73,25 +97,35 @@ export const AuralizationPlot = ({
                 label="Audio Options"
                 isOptionEqualToValue={(option, value) => option.id === value.id}
                 onChange={( _, value) => {
-                    console.log(value);
                     const newSelectedOption = audioOptions?.find((audioOption) => audioOption.id === value?.id);
                     if (newSelectedOption) {
                         setSelectedAudioOption(newSelectedOption);
                     }
+                    setAuralizationStatus('');
+                    setLoadingAuralization(false);
+                    setAuralizationId(0);
                 }}
             />
             {
                 selectedAudioOption && <ListItem
                     key={selectedAudioOption.id}
                     secondaryAction={
-                        <IconButton edge="end" aria-label="delete"  onClick={() => {
-                            if (simulationId && selectedAudioOption.id) {
-                                postAuralization({simulationId: simulationId, audioOptionId: selectedAudioOption.id})
-                            }}
-                        }>
-                            <PlayCircleIcon/>
-                        </IconButton>}
-                    >
+                        !loadingAuralization ? <span>
+                            <span>{auralizationStatus}</span>
+                            <IconButton edge="end" aria-label="delete"  onClick={() => {
+                                if (simulationId && selectedAudioOption.id) {
+                                    postAuralization({simulationId: simulationId, audioOptionId: selectedAudioOption.id})
+                                }}
+                            }>
+                                <PlayCircleIcon/>
+                            </IconButton>
+                        </span> : <div>
+                            <span>{auralizationStatus}</span>
+                            <CircularProgress size={24} style={{
+                                marginLeft: '10px'
+                            }}/>
+                        </div>
+                    }>
                     <ListItemAvatar>
                     <Avatar>
                         <FolderIcon />
---
+++ src/hooks/Auralization/index.ts
@@ -0,0 +1,2 @@
+export * from './useGetAuralizationStatus';
+export * from './useAudioOption';
